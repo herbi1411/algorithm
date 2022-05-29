e797821a0dc06d36a9949dbed4abffd6358f21f5
herbi <minsoo9615@naver.com>
2022-05-29 10:41:08 +0900

    05.29

---
+++ 그리디 알고리즘/1931(회의실 배정)/1931(회의실 배정)(실패).py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+for _ in range(N):
+    val = tuple(map(int, input().split()))
+    arr.append(val)
+arr.sort()
+answer = 0
+maxval = 0
+
+for a, b in arr:
+    if a > maxval:
+        answer += 1
+        maxval = b
+    elif maxval > b:
+        maxval = b
+
+print(answer)
